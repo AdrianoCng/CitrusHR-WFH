da2e468a139c9c0e62029fdbe775bd601e406107
Adriano Cangiamila <adriano.c@teamcreation.co.uk>
2024-03-07 17:12:18 +0000

    refactor

--- Login_manager.py
+++ Login_manager.py
@@ -5,8 +5,9 @@ from utils import parse_html
 from getpass import getpass
 
 class Login_manager:
-    url = 'https://system.citrushr.com/Login'
-    accounts_file_path = './accounts.json'
+    URL = 'https://system.citrushr.com/Login'
+    ACCOUNTS_FILE_PATH = './accounts.json'
+    KEY_FILE_PATH = './key.key'
     logged_in = False
     user_id = ''
     is_saved_account = False
@@ -27,17 +28,17 @@ class Login_manager:
                     '__RequestVerificationToken': request_verification_token
                 }
 
-                self.session.post(self.url, data)
+                self.session.post(self.URL, data)
 
+                # Successfully Login
                 if 'HrCogAuth' in self.session.cookies.keys():
                     self.user_id = self.get_user_id()
                     self.session.headers.update({'user_id': self.user_id})
                     self.logged_in = True
+                    print('Login successful')
 
                     if not self.is_saved_account:
                         self.save_credentials(username, password)
-
-                    print('Login successful')
                 else:
                     print('Email or password incorrect. Please try again')
             except ValueError as e:
@@ -46,26 +47,39 @@ class Login_manager:
 
     def get_credentials(self):
         username = input('Enter username: ')
-        try:
-            with open(self.accounts_file_path, 'r') as file:
-                accounts = json.load(file)
-                for account in accounts:
-                    if 'username' not in account or account['username'] != username:
-                        continue
-                    
-                    print('Account found')
-                    self.is_saved_account = True
-                    password = self.fernet.decrypt(account['password'].encode()).decode()
+        saved_password = self.get_saved_password(username)
 
-                    return username, password
-        except FileNotFoundError:
-            pass
+        if saved_password:
+            print('Account found')
+            self.is_saved_account = True
+            return username, saved_password
 
         password = getpass('Enter password: ')
         return username, password
+    
+    def save_credentials(self, username: str, password: str):
+        preference = input('Do you want to save this credentials? (y/n): ')
+        if preference.lower() == 'y':
+            encrypted_password = self.fernet.encrypt(password.encode()).decode()
+            credentials = {
+                'username': username,
+                'password': encrypted_password
+            }
+
+            try:
+                with open(self.ACCOUNTS_FILE_PATH, 'r') as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                data = []
+
+            data.append(credentials)
+
+            with open(self.ACCOUNTS_FILE_PATH, 'w') as file:
+                json.dump(data, file)
+            print('Credentials saved successfully')
 
     def get_verification_token(self):
-        html = self.session.get(self.url).text
+        html = self.session.get(self.URL).text
         soup = parse_html(html)
         token = soup.find('input', {'name': '__RequestVerificationToken'})['value']
         return token
@@ -77,35 +91,29 @@ class Login_manager:
         user_id = soup.find('body')['data-uid']
         return user_id
     
-    def save_credentials(self, username: str, password: str):
-        encrypted_password = self.fernet.encrypt(password.encode()).decode()
-        credentials = {
-            'username': username,
-            'password': encrypted_password
-        }
-        
-        preference = input('Do you want to save this credentials? (y/n): ')
-        if preference.lower() == 'y':
-            try:
-                with open(self.accounts_file_path, 'r') as file:
-                    data = json.load(file)
-            except FileNotFoundError:
-                data = []
-
-            data.append(credentials)
-
-            with open(self.accounts_file_path, 'w') as file:
-                json.dump(data, file)
-            print('Credentials saved successfully')
-
     def load_key(self):
-        path = './key.key'
         try:
-            with open(path, 'rb') as file:
+            with open(self.KEY_FILE_PATH, 'rb') as file:
                 key = file.read()
         except FileNotFoundError:
-            key = Fernet.generate_key()
-            with open(path, 'wb') as file:
+            with open(self.KEY_FILE_PATH, 'wb') as file:
+                key = Fernet.generate_key()
                 file.write(key)
 
         return key
+
+    def get_saved_password(self, username: str):
+        try:
+            with open(self.ACCOUNTS_FILE_PATH, 'r') as file:
+                data = json.load(file)
+                for account in data:
+                    if 'username' not in account or account['username'] != username:
+                        continue
+
+                    password = self.fernet.decrypt(account['password'].encode()).decode()
+
+                    return password
+        except FileNotFoundError:
+            pass
+    
+        return None
