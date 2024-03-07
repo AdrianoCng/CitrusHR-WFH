ebcb8860746df4c2409a7e10ae9544777b9fbb57
Adriano Cangiamila <adriano.c@teamcreation.co.uk>
2024-03-07 15:47:03 +0000

    Implement option to save credentials for automatic login

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from requests import Session
 from Login_manager import Login_manager
 from Request_handler import Request_handler
 
-
-if __name__ == '__main__':
+def main():
     session = Session()
     login_manager = Login_manager(session)
     request_handler = Request_handler(session)
 
     login_manager.login()
     request_handler.request_wfh()
+
+if __name__ == '__main__':
+    main()
